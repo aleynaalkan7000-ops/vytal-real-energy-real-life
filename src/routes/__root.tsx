c77740717613b00791eb4fd306ba8bf56327f3a4
Naelo Mbonu <nmbonu@stud.hs-heilbronn.de>
2026-06-09 00:51:17 +0200

    Sprache für SEO angepasst

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -113,7 +113,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 
 function RootShell({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="de">
+    <html lang="en">
       <head>
         <HeadContent />
       </head>
